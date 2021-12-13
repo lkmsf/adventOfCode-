8bae87447274c50f5b5338fd190b09b9838c697b
Lauren <lkmsf@yahoo.com>
2021-12-12 18:12:54 -0800

    day 8 but fancier

--- 2021/day8/day8_fancier.py
+++ 2021/day8/day8_fancier.py
@@ -8,7 +8,7 @@ with open("8.in") as f:
     inp = f.read().strip()
     inp = inp.splitlines() 
 
-inp = [[a.split() for a in x.split(" | ")] for x in inp]
+inp = [[["".join(sorted(x)) for x in a.split()] for a in x.split(" | ")] for x in inp]
 
 letters = "abcdefg"
 nums = {0: "abcefg", 1:"cf", 2:"acdeg", 3:"acdfg", 4:"bcdf", 5:"abdfg", 6:"abdefg", 7:"acf", 8:"abcdefg", 9:"abcdfg"}
@@ -19,7 +19,7 @@ def findMapping(sig):
         currMapping = {oldC: newC for oldC, newC in zip(letters, opt)}
         mapping = {}
         for i in range(10):
-            wouldBe = "".join(sorted(currMapping[x] for x in nums[i]))
+            wouldBe = "".join(sorted(map(currMapping.get, nums[i])))
             if wouldBe not in sig:
                 break
             mapping[wouldBe] = i
@@ -28,9 +28,8 @@ def findMapping(sig):
 
 total = 0
 for signals, out in inp:
-    signals = ["".join(sorted(x)) for x in signals]
     mapping = findMapping(signals)
-    output = "".join([str(mapping["".join(sorted(x))]) for x in out]) 
+    output = "".join(map(str, map(mapping.get, out))) 
     total += int(output)
 
 
